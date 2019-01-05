356b3347ada4fde9c44c666c242ebadb3c54dc87
John Seuren <john.m.seuren@gmail.com>
2019-01-05 17:31:54 +1100

    comments

--- src/app/carousel-basic/carousel-basic.component.ts
+++ src/app/carousel-basic/carousel-basic.component.ts
@@ -6,9 +6,13 @@ import { SlideType } from '../models/slide-type';
 import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment/moment';
 import { CountdownClockSlide } from '../models/countdown-clock/countdown-clock-slide';
+
 const intervalTime: number = 5000;
 const slideMover = interval(intervalTime);
+//check every second of a slide needs to be forced to how
 const secondsCounter = interval(1000);
+const slideRetreiver = interval(intervalTime);
+
 @Component({
   selector: 'ngb-carousel-basic',
   templateUrl: './carousel-basic.component.html',
@@ -18,6 +22,7 @@ const secondsCounter = interval(1000);
 export class CarouselBasicComponent implements AfterViewInit {
   @ViewChild(NgbCarousel) carousel: NgbCarousel;
 
+  //Allow slide type enum to be used in templates
   SlideType: any = SlideType;
   slides: Array<ISlide>;
   slidesFromServer: Array<ISlide>;
@@ -31,55 +36,71 @@ export class CarouselBasicComponent implements AfterViewInit {
     config.keyboard = false;
     config.pauseOnHover = false;
     config.interval = 172800000; //2 days....we want to custom control the sliding
-
-    this.getSlides().subscribe(result => {
-      this.slidesFromServer = result;
-      this.slides = this.getValidSlides();
-    });
-
   }
 
+  //get the array of slides from the server
   getSlides(): Observable<Array<ISlide>> {
     return this._http.get<Array<ISlide>>('./assets/sampleSlides.json');
   }
 
   ngAfterViewInit() {
-    slideMover.subscribe(() =>
-      this.displayNextSlide()
-    );
 
+    this.getSlides().subscribe(result => {
+      this.slidesFromServer = result;
+      this.displayNextSlide();
+    });
+
+    //this is here to rotate through slides
+    slideMover.subscribe(() => {
+      this.displayNextSlide();
+    });
+
+    //This is here to check if a slide needs to show at a specific time
     secondsCounter.subscribe(() => {
       this.checkForForcedSlideDisplay();
-    })
+    });
+
+    //half way through a slide retireveal get updated Slides from server
+    setTimeout(() => {
+      slideRetreiver.subscribe(() => {
+        this.getSlides().subscribe(result => {
+          this.slidesFromServer = result;
+        });
+      });
+    }, intervalTime / 2)
   }
 
+  //filter out any slide which may no longer be valid but is still in array or 
+  //is being sent down by the server
   private getValidSlides(): Array<ISlide> {
     return this.slidesFromServer.filter(x => {
-      if(x.hasOwnProperty('CountdownEndTime')) {
+      if (x.hasOwnProperty('CountdownEndTime')) {
         //if it is a countodown slide, has the time to countdown to
         //already passed ? If so remove slide
         if (moment() > moment((<CountdownClockSlide>x).CountdownEndTime)) {
           return false;
-        } 
-      } 
+        }
+      }
       return true;
     });
   }
 
   //Is there a slide that must be displayed at a partiucalr time ?
   private checkForForcedSlideDisplay(): boolean {
+    //filter out the slides that have a forced show time
     var forcedSlides = this.slidesFromServer.filter(x => {
-      if(x.ForceSlideToShowAtTime) {
+      if (x.ForceSlideToShowAtTime) {
         var forceTime = moment(x.ForceSlideToShowAtTime);
-        var endTime = moment(x.ForceSlideToShowAtTime).add(x.SecondsToForceShowFor,'seconds');
+        var endTime = moment(x.ForceSlideToShowAtTime).add(x.SecondsToForceShowFor, 'seconds');
         //if the forced time has passed but end forced time is less than now
-        if(forceTime < moment() && endTime > moment()) {
+        if (forceTime < moment() && endTime > moment()) {
           return true;
         }
-      } 
+      }
       return false;
     });
-    if(forcedSlides && forcedSlides.length){
+    //if tehre are nay forced slides
+    if (forcedSlides && forcedSlides.length) {
       this.slides = forcedSlides;
       //there may bemultipe configured so only allow them to rotate through until they are invalid
       var int = this.getRandomInt(0, this.slides.length - 1);
@@ -90,19 +111,22 @@ export class CarouselBasicComponent implements AfterViewInit {
     }
   }
 
+  //Randomly display a slide from teh array (if there are
+  //no slides that need to be forced to be displayed)
   private displayNextSlide(): void {
     //If a slide needs to be forced to be dispslayed due to a specified time sent down
     //then let that slide be displayed until it needs to
-    if(!this.checkForForcedSlideDisplay()) {
+    if (!this.checkForForcedSlideDisplay()) {
       //filter out anyslides that may no longer be valid to display
       this.slides = this.getValidSlides();
-      
+
       var int = this.getRandomInt(0, this.slides.length - 1);
       this.carousel.select(this.slides[int].SlideId);
     }
-    
+
   }
 
+  //get a random number so as to randmise the display of the tiles
   private getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
