4a53391aa4c78b25e99bda0fa840943fc0b25f89
John Seuren <john.m.seuren@gmail.com>
2019-06-16 14:11:31 +1000

    Check of isActive Slide change before setting video properties

--- src/app/slides/video/video.component.ts
+++ src/app/slides/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 import { VideoSlide } from 'src/app/models/video/video-slide';
 
 @Component({
@@ -6,28 +6,40 @@ import { VideoSlide } from 'src/app/models/video/video-slide';
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
-export class VideoComponent implements AfterViewInit {
+export class VideoComponent implements OnChanges {
 
   @Input() model: VideoSlide;
   @Input() isActiveSlide: boolean;
   @Output() duration = new EventEmitter<number>();
   @ViewChild('videoPlayer') videoplayer: ElementRef;
-
-  ngAfterViewInit(): void {
-    var duration = this.duration;
-    this.videoplayer.nativeElement.addEventListener('loadeddata', function (_event: any) {
-      if (this.videoplayer.nativeElement.readyState >= 2) {
-        if (this.isActiveSlide) {
-
-          this.videoplayer.nativeElement.play().then(function () {
-            duration.emit(Math.ceil(_event.target.duration));
-          })
-            .catch(function (error) {
-              //something happened so give a duration of 1 second so carousle moves onto next slide
-              duration.emit(Math.ceil(1));
-            });
-        }
-      }
-    }.bind(this)); //Bind the current scope (this) to inner event listener
+  public fileName: string;
+  ngOnChanges(changes: SimpleChanges) {
+    const activeSlide: SimpleChange = changes.isActiveSlide;
+    if (activeSlide.currentValue === true) {
+      this.fileName = this.model.Filename;
+      this.videoplayer.nativeElement.src = this.model.Filename;
+      this.videoplayer.nativeElement.addEventListener('loadeddata', function (_event: any) {
+        this.startVideoPlayer(_event);
+      }.bind(this));
+    } else {
+      this.fileName = '';
+      this.videoplayer.nativeElement.pause()
+      this.videoplayer.nativeElement.removeEventListener('loadeddata', this.startVideoPlayer)
+    }
   }
+  private startVideoPlayer(_event: any) {
+    var duration = this.duration;
+
+    if (this.videoplayer.nativeElement.readyState >= 2) {
+      this.videoplayer.nativeElement.play().then(function () {
+        duration.emit(Math.ceil(_event.target.duration));
+      })
+        .catch(function (error) {
+          //something happened so give a duration of 1 second so carousle moves onto next slide
+          duration.emit(Math.ceil(1));
+        });
+    }
+  }
+
+
 }
