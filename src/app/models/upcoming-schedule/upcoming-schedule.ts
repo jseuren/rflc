7875426472ba0ec9eacbfa551b050579d2e499b8
John Seuren <john.m.seuren@gmail.com>
2019-06-30 15:33:06 +1000

    Upcoming Schedule Slide

---
+++ src/app/models/upcoming-schedule/upcoming-schedule.ts
@@ -0,0 +1,11 @@
+export class UpcomingSchdule {
+    idschedule:string;
+    schedule_name: string;
+    start_datetime: Date;
+    start_time: string;
+    end_datetime: Date;
+    description: string;
+    video: string;
+    location: string;
+    timeDescription: string;
+}
--- src/app/slides/upcoming-schedule/upcoming-schedule.component.ts
+++ src/app/slides/upcoming-schedule/upcoming-schedule.component.ts
@@ -1,16 +1,61 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input, SimpleChanges, SimpleChange } from '@angular/core';
 import { UpcomingSchduleSlide } from 'src/app/models/upcoming-schedule/upcomig-schedule-slide';
+import { HttpClient } from '@angular/common/http';
+import { UpcomingSchdule } from 'src/app/models/upcoming-schedule/upcoming-schedule';
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'slide-upcoming-schedule',
   templateUrl: './upcoming-schedule.component.html',
   styleUrls: ['./upcoming-schedule.component.css']
 })
-export class UpcomingScheduleComponent implements OnInit {
+export class UpcomingScheduleComponent implements OnChanges {
+ 
   @Input() model: UpcomingSchduleSlide;
-  constructor() { }
+  @Input() isActiveSlide: boolean;
+  schedule: Array<UpcomingSchdule>;
+  constructor(private _http: HttpClient) { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    const activeSlide: SimpleChange = changes.isActiveSlide;
+    
+    if(activeSlide) {
+      if (activeSlide.currentValue === true) {
+
+        this.getSchedule().then(result => {
+          result.forEach(function(item){
+            item.timeDescription = this.getTimeParamter(item);
+            item.start_time = this.getStartTimeParamter(item);
+          }, this);
+          this.schedule = result;
+        });
+
+      } else {
+        this.schedule = [];
+      }
+    }
+  }
+
+  getStartTimeParamter(scheduledItem) {
+   return moment(scheduledItem.start_datetime, 'YYYY-MM-DD HH:mm:ss').format('HH:mm a');
+
+  }
+
+  getTimeParamter(scheduledItem) {
+    let eventTime = moment(scheduledItem.start_datetime, 'YYYY-MM-DD HH:mm:ss').unix();
+    let currentTime = moment().unix();
+    let diffTime = eventTime - currentTime;
+    let duration = moment.duration(diffTime, 'minutes');
+    if(diffTime > 0) {
+      return duration.asMinutes() + ' minutes time';
+    } else {
+      return duration.asMinutes() + ' minutes ago';
+    }
+  }
+
+  async getSchedule(): Promise<Array<UpcomingSchdule>> {
+    const schedule = await this._http.get<Array<UpcomingSchdule>>('https://rflapp.azurewebsites.net/schedule.php').toPromise();
+    return schedule;
   }
 
 }
